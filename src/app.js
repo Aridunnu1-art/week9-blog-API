5ff83322bc21220acf6aa6267e4df7e235b7a5ea
Salau Aisha Omowunmi <salauaisha53@gmail.com>
2026-04-05 22:29:51 +0100

    Initial commit

--- src/app.js
+++ src/app.js
@@ -6,12 +6,27 @@ const RequestLogger = require('./middleware/logger.js');
 const errorhandler = require('./middleware/errorhandler.js');
 
 const app = express();
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // folder where files will be stored
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+const upload = multer({ storage });
 app.use(express.json());
 
 app.use(cors('*'));
 app.use(RequestLogger);
+app.post('/upload', upload.single("uploads"), (req, res) => {
+    console.log("body", req.body)
+console.log("file", req.file)
+res.send("Hello, from upload")
+});
 app.use('/articles', articleRoutes);
 app.use('/auth', authRoutes);
-
 app.use(errorhandler)
 module.exports = app;
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const multer = require('multer');
 const { signup, login } = require('../controllers/user.controller.js');
 
 const  router = express.Router();
-
 router.post('/signup', signup);
 router.post('/login', login);
 
